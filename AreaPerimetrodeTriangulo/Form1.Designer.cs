029cdf083c9a38d3a95b9659ac74fd36e7d5aec7
Your Name <daylane86@gmail.com>
2022-07-05 13:09:17 -0300

    Adicionar arquivos de projeto.

---
+++ AreaPerimetrodeTriangulo/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace AreaPerimetrodeTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.resulArea = new System.Windows.Forms.Label();
+            this.resulPerimetro = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Altura:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Área:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Perímetro:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(64, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Cálculo";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(64, 51);
+            this.txtBase.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(116, 22);
+            this.txtBase.TabIndex = 5;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(64, 124);
+            this.txtAltura.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(116, 22);
+            this.txtAltura.TabIndex = 6;
+            // 
+            // resulArea
+            // 
+            this.resulArea.AutoSize = true;
+            this.resulArea.Location = new System.Drawing.Point(307, 57);
+            this.resulArea.Name = "resulArea";
+            this.resulArea.Size = new System.Drawing.Size(0, 16);
+            this.resulArea.TabIndex = 7;
+            // 
+            // resulPerimetro
+            // 
+            this.resulPerimetro.AutoSize = true;
+            this.resulPerimetro.Location = new System.Drawing.Point(307, 130);
+            this.resulPerimetro.Name = "resulPerimetro";
+            this.resulPerimetro.Size = new System.Drawing.Size(0, 16);
+            this.resulPerimetro.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(90)))));
+            this.ClientSize = new System.Drawing.Size(404, 289);
+            this.Controls.Add(this.resulPerimetro);
+            this.Controls.Add(this.resulArea);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Área e Perímetro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label resulArea;
+        private System.Windows.Forms.Label resulPerimetro;
+    }
+}
+
---
+++ AreaPerimetrodeTriangulo/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaPerimetrodeTriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if(txtBase.Text != "" && txtAltura.Text != "")
+            {
+                int b, h, area, perimetro;
+                b = Convert.ToInt32(txtBase.Text);
+                h = Convert.ToInt32(txtAltura.Text);
+                area = (b * h);
+                perimetro = (2 * (b + h));
+
+                resulArea.Text = area.ToString();
+                resulPerimetro.Text = perimetro.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Insira um Valor", "Atenção");
+            }
+        }
+    }
+}
